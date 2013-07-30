59a11eb7325d7f2baf4bd8c3bb01ab4553ecc0b0
iEpic <xcraaze@gmail.com>
2013-07-30 15:24:20 +0100

    Update to v2.1
    - woo, maven
    - added /be set <level>

---
+++ src/main/java/me/iaero/blockenchant/BlockEnchant.java
@@ -0,0 +1,41 @@
+package me.iaero.blockenchant;
+
+import java.io.File;
+import me.iaero.blockenchant.commands.BlockEnchantCommand;
+import me.iaero.blockenchant.listeners.PlayerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockEnchant extends JavaPlugin {
+
+    public static BlockEnchant instance() {
+        return (BlockEnchant) Bukkit.getPluginManager().getPlugin("BlockEnchant");
+    }
+
+    private static int max;
+
+    public static int maxLevel() {
+        return max;
+    }
+
+    public static void setMaxLevel(int level) {
+        max = level;
+    }
+
+    public void onEnable() {
+        new PlayerListener();
+        new BlockEnchantCommand();
+        setupConfig();
+        super.onEnable();
+    }
+
+    private void setupConfig() {
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            getConfig().set("Level", 15);
+            saveConfig();
+        }
+        max = getConfig().getInt("Level");
+    }
+
+}
---
+++ src/main/java/me/iaero/blockenchant/listeners/PlayerListener.java
@@ -0,0 +1,29 @@
+package me.iaero.blockenchant.listeners;
+
+import me.iaero.blockenchant.BlockEnchant;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+
+public class PlayerListener implements Listener {
+
+    public PlayerListener() {
+        Bukkit.getPluginManager().registerEvents(this, BlockEnchant.instance());
+    }
+
+    @EventHandler (ignoreCancelled = true)
+    public void onItemPrepare(PrepareItemEnchantEvent event) {
+        Player player = event.getEnchanter();
+        if (!player.isOp() || !player.hasPermission("blockenchant.enchant")) {
+            for (int i = 0; i < event.getExpLevelCostsOffered().length; i++) {
+                int offered = event.getExpLevelCostsOffered()[i];
+                if (offered > BlockEnchant.maxLevel()) {
+                    event.getExpLevelCostsOffered()[i] = BlockEnchant.maxLevel();
+                }
+            }
+        }
+    }
+
+}
