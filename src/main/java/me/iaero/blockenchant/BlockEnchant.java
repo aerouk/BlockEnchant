ef4fe8c85500f47ed31f5adfb4a58808fa26f21b
Aerouk <xcraaze@gmail.com>
2014-08-29 23:57:05 +0100

    Make it work
    removed some funky whitespaces

--- src/main/java/me/iaero/blockenchant/BlockEnchant.java
+++ src/main/java/me/iaero/blockenchant/BlockEnchant.java
@@ -1,56 +1,54 @@
 package me.iaero.blockenchant;
- 
-import java.io.File;
- 
+
 import me.iaero.blockenchant.commands.BlockEnchantCommand;
 import me.iaero.blockenchant.listeners.PlayerListener;
 import me.iaero.blockenchant.utils.ChatHelper;
- 
+
 import org.bukkit.plugin.java.JavaPlugin;
- 
+
 public class BlockEnchant extends JavaPlugin {
- 
+
     private static BlockEnchant blockenchant;
     public static ChatHelper chat;
     private static int max;
- 
+
     public static BlockEnchant getInstance() {
         return blockenchant;
     }
- 
+
     public static ChatHelper getChat() {
         return chat;
     }
- 
+
     public static int maxLevel() {
         return max;
     }
- 
+
     public static void setMaxLevel(int level) {
         max = level;
     }
- 
+
     @Override
     public void onEnable() {
         blockenchant = this;
- 
+
         new PlayerListener();
         new BlockEnchantCommand();
- 
+
         setupConfig();
     }
- 
+
     @Override
     public void onDisable() {
         blockenchant = null;
     }
- 
+
     private void setupConfig() {
-        if (!getConfig().has("Level")) {
+        if (getConfig().get("Level") != null) {
             getConfig().set("Level", 15);
         }
-    
+
         max = getConfig().getInt("Level");
     }
- 
+
 }
