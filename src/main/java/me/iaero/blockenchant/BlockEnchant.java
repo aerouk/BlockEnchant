d625573d88516a78d01c48ea41573bf90e774abd
Tom B <me@iaero.me>
2014-08-29 23:52:51 +0100

    Fix structure of main class

--- src/main/java/me/iaero/blockenchant/BlockEnchant.java
+++ src/main/java/me/iaero/blockenchant/BlockEnchant.java
@@ -1,62 +1,56 @@
 package me.iaero.blockenchant;
-
+ 
 import java.io.File;
-
+ 
 import me.iaero.blockenchant.commands.BlockEnchantCommand;
 import me.iaero.blockenchant.listeners.PlayerListener;
 import me.iaero.blockenchant.utils.ChatHelper;
-
+ 
 import org.bukkit.plugin.java.JavaPlugin;
-
+ 
 public class BlockEnchant extends JavaPlugin {
-
+ 
+    private static BlockEnchant blockenchant;
+    public static ChatHelper chat;
+    private static int max;
+ 
+    public static BlockEnchant getInstance() {
+        return blockenchant;
+    }
+ 
+    public static ChatHelper getChat() {
+        return chat;
+    }
+ 
+    public static int maxLevel() {
+        return max;
+    }
+ 
+    public static void setMaxLevel(int level) {
+        max = level;
+    }
+ 
     @Override
     public void onEnable() {
         blockenchant = this;
-
+ 
         new PlayerListener();
         new BlockEnchantCommand();
-
+ 
         setupConfig();
     }
-    
+ 
     @Override
     public void onDisable() {
         blockenchant = null;
     }
-
-    private static BlockEnchant blockenchant;
-    public static ChatHelper chat;
-
-    public static BlockEnchant getInstance() {
-        return blockenchant;
-    }
-
-    public static ChatHelper getChat() {
-        return chat;
-    }
-
-    private static int max;
-
-    public static int maxLevel() {
-        return max;
-    }
-
-    public static void setMaxLevel(int level) {
-        max = level;
-    }
-
+ 
     private void setupConfig() {
-        File file = new File(getDataFolder(), "config.yml");
-
-        if ( ! file.exists()) {
-            try {
-                getConfig().set("Level", 15);
-                saveConfig();
-            } catch(Exception e) { }
+        if (!getConfig().has("Level")) {
+            getConfig().set("Level", 15);
         }
-
+    
         max = getConfig().getInt("Level");
     }
-
+ 
 }
